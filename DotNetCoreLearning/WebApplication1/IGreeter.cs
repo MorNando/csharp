8194bfb81e6d1afae4a281d0194bd8feecc9afab
Chris Morland <chrismorland@lambtonsolutions.co.uk>
2018-03-17 14:09:13 +0000

    day 1 work

---
+++ DotNetCoreLearning/WebApplication1/IGreeter.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        public string GetMessageOfTheDay()
+        {
+            return "Greetings!";
+        }
+    }
+}
